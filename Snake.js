3c9b235126b32edf3dff726dd73f6ed492504fa2
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-02-15 19:28:39 +0100

    snake

---
+++ Snake.js
@@ -0,0 +1,55 @@
+class Snake {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.xSpeed = scale * 1;
+    this.ySpeed = 0;
+
+    this.draw = () => {
+      ctx.fillStyle = "#ffffff";
+      ctx.fillRect(this.x, this.y, scale, scale);
+    };
+
+    this.update = function () {
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+
+      if (this.x > canvas.width) {
+        this.x = 0;
+      }
+
+      if (this.y > canvas.width) {
+        this.y = 0;
+      }
+
+      if (this.x < 0) {
+        this.x = canvas.width;
+      }
+
+      if (this.y < 0) {
+        this.y = canvas.width;
+      }
+    };
+
+    this.changeDirection = (direction) => {
+      switch (direction) {
+        case "Up":
+          this.xSpeed = 0;
+          this.ySpeed = -scale * 1;
+          break;
+        case "Down":
+          this.xSpeed = 0;
+          this.ySpeed = scale * 1;
+          break;
+        case "Left":
+          this.xSpeed = -scale * 1;
+          this.ySpeed = 0;
+          break;
+        case "Right":
+          this.xSpeed = scale * 1;
+          this.ySpeed = 0;
+          break;
+      }
+    };
+  }
+}
